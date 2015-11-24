1aebc7345346806c1e32eeecc7b4c4edca0d6a13
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2015-11-24 23:09:56 +0000

    Unfinished implementation of contact form/page. Commiting as a save of current work.

--- myPortfolioSite/Controllers/HomeController.cs
+++ myPortfolioSite/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using myPortfolioSite.Models;
 
 namespace myPortfolioSite.Controllers
 {
@@ -17,9 +21,11 @@ namespace myPortfolioSite.Controllers
             "work",
             "education",
             "index",
-            "projects" };
+            "projects",
+            "contactsuccess",
+        };
 
-        // GET: Home
+        [HttpGet]
         public ActionResult StaticView(string viewName)
         {
             string viewNameRequest = "Index";
@@ -30,5 +36,43 @@ namespace myPortfolioSite.Controllers
 
             return View(viewNameRequest);
         }
+
+        /* Contact form implementation ripped from:
+        http://www.mikesdotnetting.com/article/268/how-to-send-email-in-asp-net-mvc
+        */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(ContactEmailForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                string body = "Email From: {0} ({1}) Message: {2}";
+                MailMessage message = new MailMessage();
+                message.To.Add(new MailAddress("email@gmail.com"));
+                message.From = new MailAddress("email@gmail.com");
+                message.Subject = "Site contact form test";
+                // So this is quite cool; gotta remember this method...
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = false;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    NetworkCredential credential = new NetworkCredential
+                    {
+                        UserName = "email@email.com",
+                        Password = "password" // TODO: use SecurePassword instead of Password.
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 567;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("StaticView", "ContactSuccess");
+                }
+            }
+            // Notice, this return is for when modelstate.isvalid returns false.
+            return View("Contact", model);
+        }
     }
 }
+ 
---
+++ myPortfolioSite/Models/ContactEmailForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace myPortfolioSite.Models
+{
+    public class ContactEmailForm
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email")]
+        public string FromEmail { get; set; }
+        [Required, Display(Name = "Your message")]
+        public string Message { get; set; }
+    }
+}
